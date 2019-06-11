1fd9a5c7cd39111e00c4076c675d6a474295b9e2
BPREETHAMREDDY <51318133+BPREETHAMREDDY@users.noreply.github.com>
2019-06-11 11:22:03 +0530

    Add files via upload

---
+++ DCSWITCH.c
@@ -0,0 +1,36 @@
+/*
+ * dcmotorswitch.c
+ *
+ * Created: 6/11/2019 10:41:39 AM
+ * Author : HOME
+ */ 
+
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	DDRD = 0x00;
+	DDRA = 0xFF;
+	while (1)
+	{
+		if((PIND&(1<<PD0))==0)
+		{
+				PORTA = 0x01;
+				_delay_ms(1000);
+					PORTA = 0x00;
+		}
+	if((PIND&(1<<PD1))==0)
+	{
+		PORTA = 0x02;
+		_delay_ms(1000);
+			PORTA = 0x00;
+	}
+
+	
+	}
+}
+
